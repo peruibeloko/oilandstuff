d3799da2d279e0a7b57798fa67d01b29dd9744eb
peruibeloko <cmendonca32@gmail.com>
2025-02-05 21:04:47 -0300

    stuff

---
+++ src/main/java/com/peruibeloko/oilandstuff/block/custom/BoilerHeater.java
@@ -0,0 +1,11 @@
+package com.peruibeloko.oilandstuff.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class BoilerHeater extends Block {
+    public BoilerHeater(Properties properties) {
+        super(properties);
+    }
+
+
+}
